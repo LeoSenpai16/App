2b60d3f54f5087ea8cef8f6679741bc9caff0127
LeoSenpai16 <leonel_vivar2002@hotmail.com>
2026-09-19 13:10:45 -0600

    base de datos

---
+++ database/002_seed.sql
@@ -0,0 +1,29 @@
+INSERT INTO roles (nombre)
+VALUES
+('chef'),
+('mesero');
+
+INSERT INTO mesas (numero)
+VALUES
+(1),
+(2),
+(3),
+(4),
+(5),
+(6);
+
+INSERT INTO productos (nombre, precio)
+VALUES
+('Orden de asada', 130.00),
+('Media orden', 80.00),
+('Taco de asada', 20.00),
+('Taco mixto', 22.00),
+('Quesadilla', 40.00),
+('Lorenza', 40.00),
+('Hamburguesa con papas', 80.00),
+('Hamburguesa sin papas', 60.00),
+('Orden de papas', 60.00),
+('Media orden de papas', 30.00),
+('Plato de frijol', 15.00),
+('Refresco', 30.00),
+('Agua de chía', 20.00);
